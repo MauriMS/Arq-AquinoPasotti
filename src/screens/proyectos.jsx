7ac802483259762d4c0232e323744b350baadc2e
Mauri <95150798+MauriMS@users.noreply.github.com>
2026-09-11 13:53:44 -0300

    Vista proyecto agregada

--- src/screens/proyectos.jsx
+++ src/screens/proyectos.jsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import './proyectos.css';
+import { proyectos } from '../Data/proyectos.js';
+
 function Proyectos() {
   return (
-    <div>
-      <h1>Proyectos</h1>
+    <div className="proyectos-page">
+      <div className="proyectos-container">
+        <h1 className="proyectos-title">proyectos</h1>
+
+        <div className="proyectos-grid">
+          {proyectos.map((p) => (
+            <Link to={`/proyectos/${p.id}`} className="proyecto-card" key={p.id}>
+              <img src={p.cover} alt={p.nombre} className="proyecto-cover" />
+              <div className="proyecto-overlay">
+                <span>{p.nombre}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
