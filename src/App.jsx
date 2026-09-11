d2ba43a9c8f9a6f63b109c3e0640eb5e093d0ccb
Mauri <95150798+MauriMS@users.noreply.github.com>
2026-09-11 14:00:50 -0300

    varios cambios menores

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./screens/home.jsx";
 import Proyectos from "./screens/proyectos.jsx";
+import ProyectoDetalle from "./screens/proyectodetalle";
 import Oficina from "./screens/oficina.jsx";
+import Off2 from "./screens/pruebas/off2.jsx";
 import Novedades from "./screens/novedades.jsx";
+import NovedadDetalle from "./screens/novedadDetalle";
 import Contacto from "./screens/contacto.jsx";
 import Home2 from "./screens/home2";
 import Nav from "./components/Nav.jsx";
@@ -19,8 +22,11 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home2" element={<Home2 />} />
         <Route path="/proyectos" element={<Proyectos></Proyectos>}></Route>
+        <Route path="/proyectos/:id" element={<ProyectoDetalle />}></Route>
         <Route path="/oficina" element={<Oficina></Oficina>}></Route>
+        <Route path="/off2" element={<Off2 />}></Route>
         <Route path="/novedades" element={<Novedades></Novedades>}></Route>
+        <Route path="/novedades/:id" element={<NovedadDetalle />}></Route>
         <Route path="/contacto" element={<Contacto></Contacto>}></Route>
       </Routes>
     </BrowserRouter>
