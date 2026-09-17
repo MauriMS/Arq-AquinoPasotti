5871a52a41ae8b3a79389d4934b37956ab2a5348
Mauri <95150798+MauriMS@users.noreply.github.com>
2026-09-17 18:44:14 -0300

    Pag legales creada

---
+++ src/screens/legales.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './legales.css';
+
+function Legales() {
+  return (
+    <div className="legales-page">
+      <div className="legales-container">
+        <h1 className="legales-title">legales</h1>
+
+        <div className="legales-content">
+          
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit similique cum ipsum fugiat voluptatibus harum error neque, hic nulla, at quis quas illum, cumque quo sed? Eum, veniam. Eligendi, et!
+            lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit similique cum ipsum fugiat voluptatibus harum error neque, hic nulla, at quis quas illum, cumque quo sed? Eum, veniam. Eligendi, et!
+
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Legales;
