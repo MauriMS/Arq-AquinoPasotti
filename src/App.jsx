c441a6890dea7deb6ac81a4c0dec543b93efdddc
Mauri <95150798+MauriMS@users.noreply.github.com>
2026-09-18 16:04:43 -0300

    cambios para pre-visualizacion3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./screens/home.jsx";
 import Proyectos from "./screens/proyectos.jsx";
