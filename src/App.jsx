6b6210d25752caf660a1a3fa8f956202f013e01d
Mauri <95150798+MauriMS@users.noreply.github.com>
2026-09-18 15:00:02 -0300

    cambios para pre-visualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Arq-AquinoPasotti">
       <Nav></Nav>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
