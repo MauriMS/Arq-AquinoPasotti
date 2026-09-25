e7d96af8067e7d093aa6ff0ecf3d8d4d65b4100f
Mauri <95150798+MauriMS@users.noreply.github.com>
2026-09-25 11:48:21 -0300

    cambios en legales

--- src/screens/legales.jsx
+++ src/screens/legales.jsx
@@ -16,21 +16,21 @@ function Legales() {
         </p>
 
         <div className="legales-section">
-          <h2>1- garantías y responsabilidad.</h2>
+          <h7>1- garantías y responsabilidad.</h7>
           <p>
             si bien hacemos todo lo posible para garantizar la precisión del contenido de este sitio web, este se proporciona "tal cual" y aquino pasotti consultora no ofrece garantías en relación con la exactitud o integridad de la información que contiene. si bien el contenido de este sitio se proporciona de buena fe, no garantizamos que la información se mantenga actualizada, sea veraz y no engañosa, ni que este sitio esté siempre (o nunca) disponible para su uso. no garantizamos que los servidores que hacen posible el acceso a este sitio web estén libres de errores, virus o fallos, y usted acepta que es su responsabilidad tomar las medidas adecuadas para la protección contra dichas amenazas.
           </p>
         </div>
 
         <div className="legales-section">
-          <h2>2- uso del contenido.</h2>
+          <h7>2- uso del contenido.</h7>
           <p>
             se permite la duplicación o copia del contenido para su distribución por cualquier medio, siempre y cuando se mencione la fuente de origen es decir de “aquino pasotti consultora". aquino pasotti consultora fomenta y permite los enlaces de texto a su sitio web, siempre que: (a) el propósito del enlace no sea perjudicar la reputación de aquino pasotti consultora ni de sus clientes ni proveedores de servicios; (b) al activarse, el enlace se abra a pantalla completa y no como una ventana emergente enmarcada en el sitio web enlazado; y (c) aquino pasotti consultora se reserva el derecho de revocar el consentimiento para cualquier enlace en cualquier momento, a su entera discreción, modificando este acuerdo. caso contrario aquino pasotti consultora indicara el material que no permita su duplicación, copia o acceso.
           </p>
         </div>
 
         <div className="legales-section">
-          <h2>3- información personal.</h2>
+          <h7>3- información personal.</h7>
           <p>
             aquino pasotti consultora no recopila información personal (por ejemplo: nombre, dirección o número de teléfono, etc) a menos que usted nos la proporcione voluntariamente por correo electrónico. aquino pasotti consultora no comparte información personal con terceros. cuando visita este sitio web, aquino pasotti consultora recopila automáticamente cierta información, como: – dominio, país, dirección ip – navegador, plataforma, resolución – páginas de entrada y salida, referencias – fecha, hora, términos de búsqueda y motores de búsqueda. esta es una práctica estándar en sitios web y solo se utiliza para evaluar cómo aquino pasotti consultora puede diseñar el sitio para satisfacer mejor sus necesidades.
           </p>
